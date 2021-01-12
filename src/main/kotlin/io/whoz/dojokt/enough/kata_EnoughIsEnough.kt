47206d44363782f9818d994a92b31407333b68b7
Vincent Couturier <vincent.couturier@whoz.com>
2021-01-12 18:28:52 +0100

    complete enough is enough kata https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/kotlin

--- src/main/kotlin/io/whoz/dojokt/enough/kata_EnoughIsEnough.kt
+++ src/main/kotlin/io/whoz/dojokt/enough/kata_EnoughIsEnough.kt
@@ -2,8 +2,15 @@ package io.whoz.dojokt
 
 // https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/kotlin
 object EnoughIsEnough {
-    fun deleteNth(elements:IntArray, maxOcurrences:Int):IntArray {
-        //Code here ;)
-        return IntArray(0)
+    fun deleteNth(elements: IntArray, maxOccurrences: Int): IntArray {
+        val itemsCount = mutableMapOf<Int, Long>()
+        return if (maxOccurrences <= 0)
+            IntArray(0)
+        else
+            elements.distinctBy { element ->
+            val itemCount = (itemsCount[element] ?: 0) + 1
+            itemsCount[element] = itemCount
+            if (itemCount < maxOccurrences) "$element $itemCount" else "$element $maxOccurrences"
+        }.toIntArray()
     }
 }
