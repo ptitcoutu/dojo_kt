bdb6ea1b41228a8753e82669c8fbc7663c48e841
Vincent Couturier <vincent.couturier@whoz.com>
2021-03-23 18:10:58 +0100

    a another test on neo4j cypher DSL with relationship

--- src/test/kotlin/io/whoz/dojokt/TestNeo4jDSL.kt
+++ src/test/kotlin/io/whoz/dojokt/TestNeo4jDSL.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.neo4j.cypherdsl.core.Cypher
 import org.neo4j.cypherdsl.core.Cypher.create
 import org.neo4j.cypherdsl.core.Cypher.match
+import org.neo4j.cypherdsl.core.RelationshipPattern
 import org.neo4j.cypherdsl.core.Statement
 import org.neo4j.cypherdsl.core.StatementBuilder
 import org.neo4j.cypherdsl.core.renderer.Renderer
@@ -47,6 +48,37 @@ class TestNeo4jDSL {
                 assertEquals(transaction.run(cypherRenderer.render(statement)).list { record ->
                     record.asMap().toString()
                 }, listOf("{m=node<0>}"))
+                transaction.commit()
+            }
+        }
+    }
+
+    @Test
+    fun testRelationship() {
+        val m = Cypher.node("Movie2").named("m")
+        val n = Cypher.node("Movie3").named("n")
+        val r = m.relationshipTo(n, "IS_A").named("r")
+        val statement = match(m, r)
+            .returning(r)
+            .build()
+
+        assertEquals("MATCH (m:`Movie2`), (m)-[r:`IS_A`]->(n:`Movie3`) RETURN r", cypherRenderer.render(statement))
+
+        val driver = GraphDatabase.driver(neo4jContainer.boltUrl)
+        driver.session().use { session ->
+            session.beginTransaction().use { transaction ->
+                assertEquals(transaction.run(cypherRenderer.render(statement)).list { record ->
+                    record.asMap()
+                }, emptyList<Map<String,Any>>())
+                val createStmt : StatementBuilder.BuildableStatement = create(r)
+                val createStatement: Statement = createStmt.build()
+                val createQuery = cypherRenderer.render(createStatement)
+                transaction.run(
+                    createQuery
+                )
+                assertEquals(transaction.run(cypherRenderer.render(statement)).list { record ->
+                    record.asMap().toString()
+                }, listOf("{r=relationship<0>}"))
                 transaction.commit()
             }
         }
